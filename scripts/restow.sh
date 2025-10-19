f5323e07fe628ccc095c3c4436742e6ab191c319
Schizzle13 <franktrani7887@gmail.com>
2025-10-19 00:17:01 -0400

    dotfiles

--- scripts/restow.sh
+++ scripts/restow.sh
@@ -1,7 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-base_dir="$(cd -- "$(dirname -- "$0")/.." && pwd -P)"
+base_dir="$(cd -- "$(dirname -- "$0")/../config" && pwd -P)"
 cd "$base_dir"
 
-stow -v -t "$HOME" -R config
+for dir in *; do
+    if [ -d "$dir" ]; then
+        echo "Stowing $dir..."
+        stow -v -t "$HOME" -R "$dir"
+    fi
+done
+echo "Restow complete."
